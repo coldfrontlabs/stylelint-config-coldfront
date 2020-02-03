e9328bd800a19672e7e1b269901956f7083e2119
Nick Milton <nmilton@coldfrontlabs.ca>
2020-02-03 11:28:05 -0500

    feat(rules): use prettier and recommended rules
    BREAKING CHANGE: stylelint 13 uses new rules that are not in older versions

--- index.js
+++ index.js
@@ -2,65 +2,13 @@
 
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-property-sort-order-smacss'
+    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss",
+    "stylelint-prettier/recommended",
+    "stylelint-config-property-sort-order-smacss"
   ],
-  plugins: [
-    'stylelint-scss'
-  ],
+  plugins: ["stylelint-scss", "stylelint-prettier"],
   rules: {
-    "at-rule-empty-line-before": [
-      "always", {
-        except: [
-          "blockless-after-same-name-blockless",
-          "first-nested",
-        ],
-        ignore: ["after-comment"],
-        ignoreAtRules: ["else"]
-      }
-    ],
-    'at-rule-no-unknown': null,
-    "block-closing-brace-newline-after": [
-      "always",
-      {
-        ignoreAtRules: ["if", "else"]
-      }
-    ],
-    'number-leading-zero': 'never',
-    'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-else-closing-brace-space-after': 'always-intermediate',
-    'scss/at-else-empty-line-before': 'never',
-    'scss/at-else-if-parentheses-space-before': 'always',
-    'scss/at-function-named-arguments': 'never',
-    'scss/at-function-parentheses-space-before': 'never',
-    'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-if-closing-brace-space-after': 'always-intermediate',
-    'scss/at-import-no-partial-leading-underscore': true,
-    'scss/at-mixin-argumentless-call-parentheses': 'always',
-    'scss/at-mixin-named-arguments': 'never',
-    'scss/at-mixin-parentheses-space-before': 'never',
-    'scss/at-rule-no-unknown': true,
-    'scss/declaration-nested-properties': 'never',
-    'scss/declaration-nested-properties-no-divided-groups': true,
-    'scss/dollar-variable-colon-newline-after': 'always-multi-line',
-    'scss/dollar-variable-colon-space-after': 'always-single-line',
-    'scss/dollar-variable-colon-space-before': 'never',
-    'scss/dollar-variable-empty-line-before': [
-      'always',
-      {
-        except: [
-          'first-nested',
-          'after-comment',
-          'after-dollar-variable'
-        ]
-      }
-    ],
-    'scss/dollar-variable-no-missing-interpolation': true,
-    'scss/double-slash-comment-whitespace-inside': 'always',
-    'scss/operator-no-newline-after': true,
-    'scss/operator-no-newline-before': true,
-    'scss/operator-no-unspaced': true,
-    'scss/selector-nest-combinators': 'always',
-    'scss/selector-no-redundant-nesting-selector': true
+    "prettier/prettier": true
   }
 }
