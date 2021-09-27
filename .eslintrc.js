12ae014b976b3e4d659778b769a6486d3ff2d031
Nick Milton <nmilton@coldfrontlabs.ca>
2021-09-27 12:06:54 -0400

    build(npm): update all dependencies and build config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
-"use strict"
-
 module.exports = {
-  root: true,
-  extends: [
-    "stylelint"
-  ]
-}
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["standard", "plugin:jsdoc/recommended", "prettier"],
+  plugins: ["jsdoc"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  ignorePatterns: ["!.*.js"],
+  rules: {},
+};
